ba80df046f4f18c508f6dc31d3b7f58df41ae2a2
Maitreya Patni <56203318+Maitreya29@users.noreply.github.com>
2021-03-17 09:01:02 +0530

    rip git.rip

--- dumpyara.sh
+++ dumpyara.sh
@@ -195,24 +195,24 @@ if [[ -n $GIT_OAUTH_TOKEN ]]; then
     git checkout -b "$branch"
     find . -size +97M -printf '%P\n' -o -name "*sensetime*" -printf '%P\n' -o -name "*.lic" -printf '%P\n' >| .gitignore
     git add --all
-    git remote add origin https://oauth2:"$GIT_OAUTH_TOKEN"@git.rip/Maitreya29/dump.git
+    git remote add origin https://oauth2:"$GIT_OAUTH_TOKEN"@gitlab.com/Maitreya29/dump.git
     git commit -asm "Add ${description}"
-    git push https://oauth2:"$GIT_OAUTH_TOKEN"@git.rip/ItsVixano/dump.git "$branch" ||
+    git push https://oauth2:"$GIT_OAUTH_TOKEN"@gitlab.com/Maitreya29/dump.git "$branch" ||
         (
             git update-ref -d HEAD
             git reset system/ vendor/
             git checkout -b "$branch"
             git commit -asm "Add extras for ${description}"
-            git push https://oauth2:"$GIT_OAUTH_TOKEN"@git.rip/Maitreya29/dump.git "$branch"
+            git push https://oauth2:"$GIT_OAUTH_TOKEN"@gitlab.com/Maitreya29/dump.git "$branch"
             git add vendor/
             git commit -asm "Add vendor for ${description}"
-            git push https://oauth2:"$GIT_OAUTH_TOKEN"@git.rip/Maitreya29/dump.git "$branch"
+            git push https://oauth2:"$GIT_OAUTH_TOKEN"@gitlab.com/Maitreya29/dump.git "$branch"
             git add system/system/app/ system/system/priv-app/ || git add system/app/ system/priv-app/
             git commit -asm "Add apps for ${description}"
-            git push https://oauth2:"$GIT_OAUTH_TOKEN"@git.rip/Maitreya29/dump.git "$branch"
+            git push https://oauth2:"$GIT_OAUTH_TOKEN"@gitlab.com/Maitreya29/dump.git "$branch"
             git add system/
             git commit -asm "Add system for ${description}"
-            git push https://oauth2:"$GIT_OAUTH_TOKEN"@git.rip/Maitreya29/dump.git "$branch"
+            git push https://oauth2:"$GIT_OAUTH_TOKEN"@gitlab.com/Maitreya29/dump.git "$branch"
         )
 else
     echo "Dump done locally."
@@ -231,7 +231,7 @@ if [[ -n "$TG_TOKEN" ]]; then
         printf "\n<b>Version:</b> %s" "$release"
         printf "\n<b>Fingerprint:</b> %s" "$fingerprint"
         printf "\n<b>Git:</b>"
-        printf "\n<a href=\"%s\">Branch</a>" "https://git.rip/Maitreya29/dump/-/tree/"$branch""
+        printf "\n<a href=\"%s\">Branch</a>" "https://gitlab.com/Maitreya29/dump/-/tree/"$branch""
     } >> "$PROJECT_DIR"/working/tg.html
     TEXT=$(< "$PROJECT_DIR"/working/tg.html)
     curl -s "https://api.telegram.org/bot${TG_TOKEN}/sendmessage" --data "text=${TEXT}&chat_id=${CHAT_ID}&parse_mode=HTML&disable_web_page_preview=True" > /dev/null
